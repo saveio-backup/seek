20e8deabd658e5a86932de4504875f5908160f6f
zoutianqi <qtz235689@163.com>
2021-04-14 14:11:08 +0800

    modified initDocs.js

--- src/main/initDocs.js
+++ src/main/initDocs.js
@@ -13,7 +13,7 @@ class InitDocs {
 				console.log("=======================");
 				const currentVersion = await versionDB.queryData("frontVersion");
 				console.log("frontVerison:", currentVersion);
-				if (vm.getNumberByFrontVersion(currentVersion) < vm.getNumberByFrontVersion("1.0.2-63")) {
+				if (vm.getNumberByFrontVersion(currentVersion) < vm.getNumberByFrontVersion("1.0.2-66")) {
 					console.log("toDosomeThing");
 					vm.compatibleOpeation(appDataPath, appName);
 				}
